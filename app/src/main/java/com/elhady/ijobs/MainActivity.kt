2a7cd85701c678e1fd45edf3afeb8f7ff9e8c588
islamelhady <i.elhady44@gmail.com>
2021-03-21 07:22:49 +0200

    Initial commit

---
+++ app/src/main/java/com/elhady/ijobs/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.elhady.ijobs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
