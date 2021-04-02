2bb388d4195c894326c0d61e255ef02bdc1e3e56
islamelhady <i.elhady44@gmail.com>
2021-04-02 23:45:57 +0200

    Del persistence module

--- app/src/main/java/com/elhady/ijobs/data/local/AppDatabase.kt
+++ app/src/main/java/com/elhady/ijobs/data/local/AppDatabase.kt
@@ -1,13 +1,11 @@
 package com.elhady.ijobs.data.local
 
-import androidx.room.Database
-import androidx.room.RoomDatabase
-import com.elhady.ijobs.data.model.Jobs
+
 
 /**
  * Created by islam elhady on 28-Mar-21.
  */
-@Database(entities = [Jobs::class], version = 1, exportSchema = false)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun jobsDao(): JobsDao
-}
+//@Database(entities = [Jobs::class], version = 1, exportSchema = false)
+//abstract class AppDatabase : RoomDatabase() {
+//    abstract fun jobsDao(): JobsDao
+//}
